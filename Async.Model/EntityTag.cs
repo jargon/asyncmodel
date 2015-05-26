52c60c252bd2b75dfe95497fee24e9b71e207d82
Søren Nguyen Boisen <snb@hivepeople.dk>
2015-05-26 10:12:05 +0200

    add: a possible model for generalized entity tags as known from http

---
+++ Async.Model/EntityTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Async.Model
+{
+    public struct EntityTag<TKey, TTag>
+    {
+        public readonly TKey key;
+        public readonly TTag tag;
+
+        public EntityTag(TKey key, TTag tag)
+        {
+            this.key = key;
+            this.tag = tag;
+        }
+    }
+}
