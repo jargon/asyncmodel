d3b401665937458765c6922b7c148766f0f24b7f
Søren Nguyen Boisen <snb@hivepeople.dk>
2015-07-07 22:21:43 +0200

    [TSS-169] NuGet requires prerelease version due to dependencies
     - fix: use a prerelease version for AssemblyInformationalVersion or NuGet
       will fail to create NuGet package due to a dependency having prerelease
       version

--- Async.Model.UnitTest/Properties/AssemblyInfo.cs
+++ Async.Model.UnitTest/Properties/AssemblyInfo.cs
@@ -1,18 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Async.Model.UnitTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Async.Model.UnitTest")]
+[assembly: AssemblyTitle("Async.Model.UnitTest")]
 [assembly: AssemblyCopyright("Copyright © Søren Nguyen Boisen 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,4 +27,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// This is needed for specifying NuGet package version since AssemblyVersion does
+// not support semantic versioning.
+[assembly: AssemblyInformationalVersion("1.0.0-beta1")]
