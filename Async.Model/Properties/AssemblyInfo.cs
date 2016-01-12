db5f55a9427dad2ef695b0334648f8eab16bfadf
Søren Nguyen Boisen <snb@hivepeople.dk>
2016-01-12 13:32:26 +0100

    Ooops forgot to increment version once again

--- Async.Model/Properties/AssemblyInfo.cs
+++ Async.Model/Properties/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.8")]
+[assembly: AssemblyVersion("1.0.0.9")]
 
 // This is needed for specifying NuGet package version since AssemblyVersion does
 // not support semantic versioning.
-[assembly: AssemblyInformationalVersion("1.0.0-beta8")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta9")]
